909fc9291b78f8513d2cf59226d46eafa068b2f0
Timur Ermolaev <timkogas@gmail.com>
2022-03-14 13:38:23 +0300

    Задание 2. Реализовал активный таб

--- task-2/js/main.js
+++ task-2/js/main.js
@@ -1,5 +1,8 @@
 $('.content-item').first().show()
+$('.item').first().addClass("active")
 $('.item').on('click', function() {
+  $('.item').removeClass("active")
+  $(this).addClass('active')
   const contentID = $(this).attr('href')
   $('.content-item').hide()
   $(`${contentID}`).show()
